fd53a0c9c823f1afa7cf9e17a9781d89ffa0a94d
Rashid Khan <github.fliplap@spamgourmet.com>
2016-10-07 13:29:44 -0700

    Revert "Switch back to non-script access to fields"
    This reverts commit 48227de5f81cdf8762498154a36edeb178c579bc.

--- server/types/search_request/lib/append_dsl_field.js
+++ server/types/search_request/lib/append_dsl_field.js
@@ -1,21 +1,16 @@
 // Insert either a `field` key, or a `script` key to an aggregation or filter/query
 // This should be used every time a field is passed, otherwise you may miss out on
-const getFieldScript = require('./get_field_script');
-const _ = require('lodash');
+var getFieldScript = require('./get_field_script');
+var _ = require('lodash');
 
 module.exports = function (field, obj, searchRequest) {
-  /*
   var result = {
     script: {
       script: getFieldScript(field, searchRequest),
       lang: 'javascript'
     }
   }
-  */
-  const result = {
-    field: field
-  };
 
   _.merge(result, obj);
   return result;
-};
+}
