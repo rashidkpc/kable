fad57898e0f2e5e88246ad395f39d1ca87f33293
Rashid Khan <github.fliplap@spamgourmet.com>
2017-03-28 16:41:08 -0700

    Direct script access vs self invoking function

--- server/types/search_request/lib/get_field_script.js
+++ server/types/search_request/lib/get_field_script.js
@@ -1,8 +1,4 @@
 module.exports = function (field, searchRequest) {
   searchRequest.scripts = searchRequest.scripts || {};
-  return searchRequest.scripts[field] || `(
-    function () {
-      return doc['${field}'].value;
-    }()
-  )`
-}
+  return searchRequest.scripts[field] || `(doc['${field}.keyword'].value)`;
+};
