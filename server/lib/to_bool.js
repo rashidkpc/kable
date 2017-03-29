11d4aa77fc88237fad1d0226bd58cfc812e4ddd8
Rashid Khan <github.fliplap@spamgourmet.com>
2017-03-29 11:05:24 -0700

    Make searching work with painless

--- server/lib/to_bool.js
+++ server/lib/to_bool.js
@@ -6,28 +6,78 @@ var path = require('path');
 
 var Parser = require('pegjs').buildParser(fs.readFileSync(path.resolve(__dirname, './search.peg'), 'utf8'));
 
-function toQuery(clause) {
+var operators = {
+  lte: '<=',
+  lt:  '<',
+  eq:  '==',
+  gt:  '>',
+  gte: '>=',
+};
+
+function toQuery(clause, scripts) {
   var query = {};
+  var scripts = scripts || {};
   if (clause.type === 'group' || clause.type === 'search') {
-    query = toBool(clause.clauses);
+    query = toBool(clause.clauses, scripts);
+  } else if (scripts[clause.field]) { // Is this a script? We can do this the fast way
+    query = scriptToQuery(`${scripts[clause.field]} ${operators[clause.type]} params.param`, clause.value);
   } else {
-    if (clause.type === 'eq') {
-      _.set(query, `match.${clause.field}`, {query: clause.value, type: 'phrase'});
-    } else if (_.contains(['lt', 'gt', 'lte', 'gte'], clause.type)) {
-      _.set(query, `range.${clause.field}.${clause.type}`, clause.value);
+    if (clause.field) {
+      query = scriptToQuery(`doc['${clause.field}'].value.contains(params.param)`, clause.value);
     } else {
-      throw 'Unknown operator: ' + clause.type;
+      query = {query_string: {query: clause.value, all_fields: true}};
     }
   }
 
+    /*
+      if (clause.type === 'eq') {
+        _.set(query, `match.${clause.field}`, {query: clause.value, type: "phrase"});
+      } else if (_.contains(['lt', 'gt', 'lte', 'gte'], clause.type)) {
+        _.set(query, `range.${clause.field}.${clause.type}`, clause.value);
+      } else {
+        throw 'Unknown operator: ' + clause.type;
+      }
+      */
+
+  logObj(query);
+
   return query;
 }
+
+function scriptToQuery(script, value) {
+  return {
+    bool: {
+      filter: {
+        script: {
+          script: {
+            inline: script,
+            params: {
+              param: value
+            },
+            lang: 'painless'
+          }
+        }
+      }
+    }
+  };
+}
+
+
+/*
+{must_not: [
+  {bool:{filter:{script:{}}}}
+]}
+*/
 
 function toBool(clauses, scripts) {
   return _.reduce(clauses, function (context, clause) {
     context.bool[clause.imperative].push(toQuery(clause, scripts));
     return context;
   }, {bool: {must: [], must_not: [], should: []}});
+}
+
+function logObj(obj) {
+  console.log(JSON.stringify(obj, null, ' '));
 }
 
 module.exports = toQuery;
@@ -40,11 +90,9 @@ function run(search) {
     return Promise.reject(Boom.badRequest(e.toString()));
   });
 }
-
 function logObj(obj, thing) {
   console.log(JSON.stringify(obj, null, ' '));
 }
-
 function dbg(expression) {
   console.log('Expression:', expression);
   var result = run(expression);
@@ -52,6 +100,5 @@ function dbg(expression) {
     logObj(result);
   });
 }
-
 dbg('+foo:bar -(+stuff -things +baz:moot) lol>10 beer>:200 beer<:100 lolstuff<1.222222')
 */
